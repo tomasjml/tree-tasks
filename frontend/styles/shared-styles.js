70a828de878ccdd83dbfadc8ec88792ebf901257
Tomas Molina <jtml.mass@gmail.com>
2021-01-22 19:47:35 -0400

    Agregar green Scrollbar

--- frontend/styles/shared-styles.js
+++ frontend/styles/shared-styles.js
@@ -1,13 +1,17 @@
 // eagerly import theme styles so as we can override them
 import '@vaadin/vaadin-lumo-styles/all-imports';
+import '@vaadin/vaadin-grid/all-imports';
+
 
 const $_documentContainer = document.createElement('template');
+
 
 $_documentContainer.innerHTML = `
 <custom-style>
 <style>
         html {
       --lumo-base-color: #483B32;
+      --lumo-base-color-darker: #3D3329;
       --lumo-primary-color: #74A57F;
       --lumo-error-color: #DB504A;
       --lumo-success-color: #FFBF00;
@@ -28,6 +32,7 @@ $_documentContainer.innerHTML = `
     [theme~="dark"] {
       --lumo-base-color: #483B32;
       --lumo-primary-color: #74A57F;
+      --lumo-primary-color-darker : #496F52;
       --lumo-error-color: #DB504A;
       --lumo-success-color: #FFBF00;
       --lumo-body-text-color: #F0E2E7;
@@ -36,10 +41,49 @@ $_documentContainer.innerHTML = `
       --lumo-success-text-color: #FFBF00;
 
     }
+    
+    *::-webkit-scrollbar {
+      width: 12px;
+      height: 15px;
+    }
+    
+    *::-webkit-scrollbar-track-piece  {
+      background-color: var(--lumo-base-color-darker);
+      border-radius: 5px;
+    }
+    
+    *::-webkit-scrollbar-thumb {
+      height: 30p#grid-wrappex;
+      background-color: var(--lumo-primary-color);
+      border-radius: 5px;
+      -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .3);
+    }
 
 </style>
 </custom-style>
 
+<dom-module id="vaadin-grid" theme-for="vaadin-grid">
+    <template>
+        <style>
+             *::-webkit-scrollbar {
+                  width: 12px;
+                  height: 15px;
+                }
+                
+                *::-webkit-scrollbar-track-piece  {
+                  background-color: var(--lumo-base-color-darker);
+                  border-radius: 5px;
+                }
+                
+                *::-webkit-scrollbar-thumb {
+                  height: 30p#grid-wrappex;
+                  background-color: var(--lumo-primary-color);
+                  border-radius: 5px;
+                  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .3);
+                }
+        </style>
+    </template>
+</dom-module>
 
 `;
 
